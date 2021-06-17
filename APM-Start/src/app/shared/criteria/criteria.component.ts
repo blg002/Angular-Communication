ad4cacb2d8ae1cedf6658614a922f7f6b5cd323b
Brad Graham <blg002@gmail.com>
2021-06-17 15:39:43 -0400

    Watching for Changes

--- APM-Start/src/app/shared/criteria/criteria.component.ts
+++ APM-Start/src/app/shared/criteria/criteria.component.ts
@@ -1,14 +1,15 @@
-import { Component, AfterViewInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'pm-criteria',
   templateUrl: './criteria.component.html',
   styleUrls: ['./criteria.component.css']
 })
-export class CriteriaComponent implements AfterViewInit {
+export class CriteriaComponent implements AfterViewInit, OnChanges {
   listFilter: string;
   @Input() displayDetail: boolean;
   @Input() hitCount: number;
+  hitMessage: string;
 
   @ViewChild('filterElement') filterElementRef: ElementRef;
 
@@ -20,4 +21,11 @@ export class CriteriaComponent implements AfterViewInit {
     }
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['hitCount'] && !changes['hitCount'].currentValue) {
+      this.hitMessage = "No Matches Found";
+    } else {
+      this.hitMessage = `Hits: ${this.hitCount}`
+    }
+  }
 }
