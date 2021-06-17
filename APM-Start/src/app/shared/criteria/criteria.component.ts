3acaa3bb3cf9dd52483cdc54df81754731327f6d
Brad Graham <blg002@gmail.com>
2021-06-17 16:36:23 -0400

    Watching for Changes get/set

--- APM-Start/src/app/shared/criteria/criteria.component.ts
+++ APM-Start/src/app/shared/criteria/criteria.component.ts
@@ -8,8 +8,23 @@ import { Component, AfterViewInit, ViewChild, ElementRef, Input, OnChanges, Simp
 export class CriteriaComponent implements AfterViewInit, OnChanges {
   listFilter: string;
   @Input() displayDetail: boolean;
-  @Input() hitCount: number;
+  // @Input() hitCount: number;
   hitMessage: string;
+  private _hitCount: number;
+  
+  get hitCount() {
+    return this._hitCount;
+  }
+
+  @Input()
+  set hitCount(value: number) {
+    this._hitCount = value;
+    if (this._hitCount) {
+      this.hitMessage = `Hits: ${this._hitCount}`
+    } else {
+      this.hitMessage = "No Matches Found";
+    }
+  }
 
   @ViewChild('filterElement') filterElementRef: ElementRef;
 
@@ -22,10 +37,10 @@ export class CriteriaComponent implements AfterViewInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['hitCount'] && !changes['hitCount'].currentValue) {
-      this.hitMessage = "No Matches Found";
-    } else {
-      this.hitMessage = `Hits: ${this.hitCount}`
-    }
+    // if (changes['hitCount'] && !changes['hitCount'].currentValue) {
+    //   this.hitMessage = "No Matches Found";
+    // } else {
+    //   this.hitMessage = `Hits: ${this.hitCount}`
+    // }
   }
 }
